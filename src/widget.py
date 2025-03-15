7a6edb8ce4da948b4842094d9c16576b133fdebb
Zarina Burceva <galazarin2016@mail.ru>
2025-03-15 23:29:50 +0500

    widget.py add get_date

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(name_and_number: str) -> str:
     """Функция возвращает маску номера карты ИЛИ счёта"""
-    split_str = name_and_number.rsplit(' ', 1)  # Разделяем с права, чтобы отделить последний элемент
+    split_str = name_and_number.rsplit(" ", 1)  # Разделяем с права, чтобы отделить последний элемент
     card_type = split_str[0]
     number = int(split_str[1])
 
@@ -14,9 +14,10 @@ def mask_account_card(name_and_number: str) -> str:
         return f"{card_type} {str(get_mask_card_number(number))}"
 
 
-print(mask_account_card("Счет 64686473678894779589"))
+def get_date(date_iso_format: str) -> str:
+    """Функция возвращает строку с датой в формате "ДД.ММ.ГГГГ" из "2024-03-11T02:26:18.671407" """
+    day = date_iso_format[8:10]
+    month = date_iso_format[5:7]
+    year = date_iso_format[0:4]
 
-# def get_date(str) -> str:
-# """Функция возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
-#
-#     return str
+    return f"{day}.{month}.{year}"
